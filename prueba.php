f69457b4ef2940e6e749889b872ca4e5bf646932
Nuria Barrios Santos <nuria.barsan@educa.jcyl.es>
2020-09-26 01:35:53 +0200

    Añadido prueba.php

---
+++ prueba.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de variables en PHP</title>
+</head>
+<body>
+    <?php
+        $a  = 137;
+        $b = "9.9";
+        $c = "ejemplos de variables PHP";
+        // impresión
+        echo $a . "<br>" . $b . "<br>" . $c;
+    ?>
+</body>
+</html>
